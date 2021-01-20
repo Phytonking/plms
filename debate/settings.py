d168a91f9a9f4b6181aefc04364875a596016587
Phytonking <avi.agola@gmail.com>
2021-01-20 14:23:32 -0800

    setting change

--- debate/settings.py
+++ debate/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'wr*r5vw346ermo6^mr$+5+jz)h85+or&r%!o&=_og%iht&p=dt6vkj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0"]
 
 
 # Application definition
